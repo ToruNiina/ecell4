2e2da13228d88b5738bedc3f5c6176d476ce06a0
ToruNiina <niina.toru.68u@gmail.com>
2019-07-02 13:03:40 +0900

    :checkered_flag: set ErrorAction = Stop when init

--- build.ps1
+++
@@ -1,7 +0,0 @@
-$ErrorActionPreference = "Stop"
-conda build conda.recipe
-# Copy the conda build to the home dir, such that it can be registerd as an artifact
-#ls C:\Miniconda36-x64\conda-bld
-mv C:\Miniconda$env:CONDA\conda-bld .
-pwd
-ls .
--- install.ps1
+++
@@ -1,4 +0,0 @@
-$ErrorActionPreference = "Stop"
-conda config --set always_yes yes --set changeps1 no
-conda update -q conda
-conda install -q conda-build anaconda-client
