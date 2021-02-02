e05b87347e1bdc874898d1dc5ecfc1554bdc1ba6
ToruNiina <niina.toru.68u@gmail.com>
2021-02-02 14:43:18 +0900

    :sparkles: update sample to check reaction

--- ecell4/ngfrd/samples/main.cpp
+++ ecell4/ngfrd/samples/main.cpp
@@ -27,10 +27,20 @@ int main()
     const ecell4::Real     volume(L * L * L);
 
     std::shared_ptr<ecell4::NetworkModel> model(new ecell4::NetworkModel());
-    ecell4::Species sp1(std::string("A"), /*r = */ 1e-2, /*D = */ 1e-2);
-    ecell4::Species sp2(std::string("B"), /*r = */ 1e-2, /*D = */ 1e-5);
+    ecell4::Species sp1(std::string("A"), /*r = */ 1e-2, /*D = */ 1.0e-2);
+    ecell4::Species sp2(std::string("B"), /*r = */ 1e-2, /*D = */ 1.0e-2);
+    ecell4::Species sp3(std::string("C"), /*r = */ 2e-2, /*D = */ 0.5e-2);
+    ecell4::Species sp4(std::string("D"), /*r = */ 1e-2, /*D = */ 1e-5);
     model->add_species_attribute(sp1);
     model->add_species_attribute(sp2);
+    model->add_species_attribute(sp3);
+    model->add_species_attribute(sp4);
+
+    const ecell4::Real k_bind   = 0.1;
+    const ecell4::Real k_unbind = 0.001;
+
+    model->add_reaction_rule(ecell4::create_binding_reaction_rule  (sp1, sp2, sp3, k_bind));
+    model->add_reaction_rule(ecell4::create_unbinding_reaction_rule(sp3, sp1, sp2, k_unbind));
 
     std::shared_ptr<ecell4::RandomNumberGenerator> rng =
         std::make_shared<ecell4::GSLRandomNumberGenerator>();
@@ -39,8 +49,10 @@ int main()
     std::shared_ptr<ecell4::ngfrd::NGFRDWorld> world =
         std::make_shared<ecell4::ngfrd::NGFRDWorld>(
                 edge_lengths, matrix_sizes, /*margin = */ 0.1, rng);
-    world->add_molecules_3D(sp1, 10);
-    world->add_molecules_2D(sp2, 10);
+    world->add_molecules_3D(sp1, 20);
+    world->add_molecules_3D(sp2, 20);
+
+    world->add_molecules_2D(sp4, 10);
 
     std::ofstream traj("traj.xyz");
     snapshot_output(traj, world);
